52d7426406c74f6974e1b68333b39eb79b134fc4
Heiko Blomenkamp <heiko.blomenkamp@gmx.de>
2022-11-08 00:02:52 +0000

    python 3.x, day 2.
    new file:   xcoral/python3/py_002/app.py
    new file:   xcoral/python3/py_002/app.py,v
    new file:   xcoral/python3/py_002/app2.py
    new file:   xcoral/python3/py_002/app2.py,v

---
+++ xcoral/python3/py_002/app.py
@@ -0,0 +1,16 @@
+# $Id: app.py,v 1.1 2022/11/07 22:56:17 heiko Exp $
+#
+# $Log: app.py,v $
+# Revision 1.1  2022/11/07 22:56:17  heiko
+# Initial revision
+#
+
+print("[01] Giraffe\nAcademy")
+print("[02] Giraffe\"Academy")
+print()
+
+print("[03]")
+phrase = "Giraffe Academy"
+print(phrase)
+
+print("[04] " + phrase + " is cool")
---
+++ xcoral/python3/py_002/app2.py
@@ -0,0 +1,16 @@
+# $Id: app2.py,v 1.1 2022/11/07 23:37:39 heiko Exp $
+#
+# $Log: app2.py,v $
+# Revision 1.1  2022/11/07 23:37:39  heiko
+# Initial revision
+#
+
+phrase = "Giraffe Academy"
+
+print("[01] " + phrase.lower())
+print("[02] " + phrase.upper())
+print("[03] phrase.isupper()")
+print(phrase.isupper())
+
+print("[04] phrase.upper().isupper()")
+print(phrase.upper().isupper())
