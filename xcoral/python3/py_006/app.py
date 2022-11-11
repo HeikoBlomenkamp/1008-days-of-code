d0426b03be93e3f32d4e9c703173dd7b337b36d8
Heiko Blomenkamp <heiko.blomenkamp@gmx.de>
2022-11-11 22:46:25 +0000

    python 3.x, day 6.
    new file:   xcoral/python3/py_006/app.py
    new file:   xcoral/python3/py_006/app.py,v
    new file:   xcoral/python3/py_006/app2.py
    new file:   xcoral/python3/py_006/app2.py,v

---
+++ xcoral/python3/py_006/app.py
@@ -0,0 +1,15 @@
+# $Id: app.py,v 1.1 2022/11/11 20:49:10 heiko Exp $
+#
+# $Log: app.py,v $
+# Revision 1.1  2022/11/11 20:49:10  heiko
+# Initial revision
+#
+
+print(3 * 4 + 5)    # 17
+print(3 * (4 + 5))  # 27
+
+print(10 % 3)  # 1
+
+myNum = 5
+print(myNum)
+print(str(myNum) + " -> my favorite number")
---
+++ xcoral/python3/py_006/app2.py
@@ -0,0 +1,16 @@
+# $Id: app2.py,v 1.1 2022/11/11 22:23:27 heiko Exp $
+#
+# $Log: app2.py,v $
+# Revision 1.1  2022/11/11 22:23:27  heiko
+# Initial revision
+#
+
+myNum = -5
+print(abs(myNum))    # 5
+
+print(pow(4, 6))     # 4096
+print(max(4, 6))     # 6
+print(min(4, 6))     # 4
+
+print(round(3.2))    # 3
+print(round(3.7))    # 4
