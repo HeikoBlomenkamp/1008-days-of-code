32031d6b2185bd67d1de84537fc094443fd6631b
Heiko Blomenkamp <heiko.blomenkamp@gmx.de>
2022-11-13 21:23:35 +0000

    python 3.x, day 8.
    new file:   xcoral/python3/py_008/app.py
    new file:   xcoral/python3/py_008/app.py,v
    new file:   xcoral/python3/py_008/app2.py
    new file:   xcoral/python3/py_008/app2.py,v

---
+++ xcoral/python3/py_008/app.py
@@ -0,0 +1,14 @@
+# $Id: app.py,v 1.1 2022/11/13 18:50:27 heiko Exp $
+#
+# $Log: app.py,v $
+# Revision 1.1  2022/11/13 18:50:27  heiko
+# Initial revision
+#
+
+from math import *
+
+
+print(floor(3.7))    # 3
+print(ceil(3.7))     # 4
+
+print(sqrt(36))      # 6.0
---
+++ xcoral/python3/py_008/app2.py
@@ -0,0 +1,10 @@
+# $Id: app2.py,v 1.1 2022/11/13 21:03:14 heiko Exp $
+#
+# $Log: app2.py,v $
+# Revision 1.1  2022/11/13 21:03:14  heiko
+# Initial revision
+#
+
+name = input("Enter your name: ")
+age = input("Enter your age: ")
+print("Hello " + name + "! You are " + age)
